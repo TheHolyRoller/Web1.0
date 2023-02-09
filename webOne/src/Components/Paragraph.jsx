6baa4f2e5478ade6e10ccc3adc120cce997c2b6b
TheHolyRoller <79761726+TheHolyRoller@users.noreply.github.com>
2023-02-09 18:35:16 +0100

    Polishing up all the integrated components. Conducting thorough tests

--- webOne/src/Components/Paragraph.jsx
+++ webOne/src/Components/Paragraph.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import ParagraphCSS from '../Styles/Paragraph.module.css'; 
-
+  
+  
 
 function Paragraph() {
   return (
-    <div>
+    <div className={ParagraphCSS.container}>
+    
+    <div className={ParagraphCSS.paragraphContainer}> 
+    
+    <p className={ParagraphCSS.paragraph}>
+    Fulfilling the Great Commission through fervent prayer, passionate worship and dedication to helping people fulfill their God-given destinies. 
+
+    </p>
+
+    </div>
       
 
     </div>
-  )
+  );
 }
 
 export default Paragraph
